2b5c11d3635d17d12b6c67e2676eaa093c282cf0
Christian Klewar <cklewar@t-online.de>
2021-03-18 19:09:45 +0100

    fix run.sh missing terminator

--- run.sh
+++ run.sh
@@ -1,2 +1,4 @@
-until $(curl --output /dev/null --silent --location --request GET --fail 'http://accountapi:8080/v1/health' --header 'Content-Type: application>
+#!/bin/sh
+until $(curl --output /dev/null --silent --location --request GET --fail 'http://accountapi:8080/v1/health' --header 'Content-Type: application/vnd.api+json');do printf '.';sleep 5;done
 go test -v ./api/client/ -coverprofile cover.out
+go run ./examples/main.go
